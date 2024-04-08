fef4ade0f8a2febf5352f1cc1746350fc75a38e5
André Azzi <andreaz005@outlook.com>
2024-04-08 14:48:46 -0400

    done

--- Labs/Codes_LAB11/Codes_LAB11/Q4/LinkedList.java
+++ Labs/Codes_LAB11/Codes_LAB11/Q4/LinkedList.java
@@ -1,40 +1,38 @@
-
-
 public class LinkedList<E> {
-  
-    //doubly linked nodes (static nested class) 
+
+    // doubly linked nodes (static nested class)
     private static class Node<E> {
-    
+
         private E value;
-    
+
         private Node<E> previous;
         private Node<E> next;
-    
-        private Node( E value, Node<E> previous, Node<E> next ) {
+
+        private Node(E value, Node<E> previous, Node<E> next) {
             this.value = value;
             this.previous = previous;
             this.next = next;
         }
     }
-  
+
     // Instance variables
     private Node<E> head;
     private int size;
-  
-    /** Constructor for creating a new linked list.
+
+    /**
+     * Constructor for creating a new linked list.
      */
     public LinkedList() {
-    
+
         // the empty list using a dummy node
-        head = new Node<E>( null, null, null );
+        head = new Node<E>(null, null, null);
         head.next = head.previous = head;
         size = 0;
     }
-  
-    
-    //  Instance methods
-  
-    /** 
+
+    // Instance methods
+
+    /**
      * Returns the number of elements currently stored in this list.
      *
      * @return the number of elements of this list.
@@ -42,135 +40,131 @@ public class LinkedList<E> {
     public int size() {
         return size;
     }// End of size
-  
+
     /**
      * Returns true if this list is empty
      *
      * @return true if this list is empty
      */
-  
+
     public boolean isEmpty() {
         return size == 0;
     }// End of isEmpty
-  
-    /** 
+
+    /**
      * Adds an element at the end of the list.
      *
      * @throws IllegalArgumentException if elem is null.
      */
-    public boolean addLast( E elem ) {
-    
-        if ( elem == null ) {
-            throw new IllegalArgumentException( "null" );
+    public boolean addLast(E elem) {
+
+        if (elem == null) {
+            throw new IllegalArgumentException("null");
         }
-    
+
         Node<E> before = head.previous;
         Node<E> after = head;
-    
-        before.next = new Node<E>( elem, before, after );
+
+        before.next = new Node<E>(elem, before, after);
         after.previous = before.next;
-    
+
         size++;
-    
+
         return true;
     }// End of addLast
-  
-    /** 
+
+    /**
      * Adds an element at the start of the list.
      *
      * @throws IllegalArgumentException if elem is null.
      */
-    public boolean addFirst( E elem ) {
-    
-        if ( elem == null ) {
-            throw new IllegalArgumentException( "null" );
+    public boolean addFirst(E elem) {
+
+        if (elem == null) {
+            throw new IllegalArgumentException("null");
         }
-    
+
         Node<E> before = head;
         Node<E> after = head.next;
-    
-        before.next = new Node<E>( elem, before, after );
+
+        before.next = new Node<E>(elem, before, after);
         after.previous = before.next;
-    
+
         size++;
-    
+
         return true;
     }// End of addFirst
-  
-    /** 
+
+    /**
      * Returns the element at the specified index; the first
      * element has the index 0.
      *
      * @return the element at the specified index.
-     * @throws IndexOutOfBoundsException if index is out of range (index < 0 || index >= size()). 
+     * @throws IndexOutOfBoundsException if index is out of range (index < 0 ||
+     *                                   index >= size()).
      */
-  
-    public E get( int index ) {
-    
-        if ( index < 0 || index > (size-1) ) {
-            throw new IndexOutOfBoundsException( Integer.toString( index ) );
-        }
-    
-        Node<E> p = head.next;
-    
-        for ( int i=0; i<index; i++ ) {
-            p = p.next;
-        }
-    
-        return p.value; 
-    }// End of get
-    
-    
-    /** 
-     * remove instance method 
-     */
-    public LinkedList<E> remove( int fromIndex, int toIndex ) {
-        Node<E> current = head;
-    
-    
-        for (int i =0; i < fromIndex ; i++) {
-            current = current.next;
-        }
-        Node<E> node1 = current;
-    
-    
-        for (int i =0; i < toIndex ; i++) {
-            current = current.next;
-        }
-        Node<E> node2 = current;
-    
-    
-        if (node1.previous != null) {
-            node1.previous.next = node2.next;
-        } else {
-            head = node2.next;
-        }
-        if (node2.next != null) {
-            node2.next.previous = node1.previous;
-        } else {
-            head.previous = node1.previous;
-        }
-    
-    
-        return this;
-    
-    
-        } // End of remove
 
-   
-    //  toString instance method
-    public String toString() {
-        StringBuffer input = new StringBuffer( "[" );
+    public E get(int index) {
+
+        if (index < 0 || index > (size - 1)) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        }
+
         Node<E> p = head.next;
-        while ( p != head ) {
-            if ( p != head.next ) {
-         input.append( "," );
-            }
-            input.append( p.value );
+
+        for (int i = 0; i < index; i++) {
             p = p.next;
         }
-        input.append( "]" );
+
+        return p.value;
+    }// End of get
+
+    /**
+     * remove instance method
+     */
+    public LinkedList<E> remove(int fromIndex, int toIndex) {
+        // VOTRE CODE VIENT ICI
+        LinkedList<E> removedElements = new LinkedList<>();
+
+        if (fromIndex < 0 || fromIndex >= size || toIndex < 0 || toIndex >= size || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException("Invalid indices");
+        }
+
+        Node<E> prev = head;
+        Node<E> current = head.next;
+
+        // Traverse to the node at fromIndex
+        for (int i = 0; i < fromIndex; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        // Remove nodes from fromIndex to toIndex
+        for (int i = fromIndex; i <= toIndex; i++) {
+            removedElements.addLast(current.value);
+            prev.next = current.next;
+            current.next.previous = prev;
+            current = current.next;
+            size--;
+        }
+
+        return removedElements;
+    }
+
+    // toString instance method
+    public String toString() {
+        StringBuffer input = new StringBuffer("[");
+        Node<E> p = head.next;
+        while (p != head) {
+            if (p != head.next) {
+                input.append(",");
+            }
+            input.append(p.value);
+            p = p.next;
+        }
+        input.append("]");
         return input.toString();
-   }// End of toString
-  
-} // End of  LinkedList
+    }// End of toString
+
+} // End of LinkedList
+
