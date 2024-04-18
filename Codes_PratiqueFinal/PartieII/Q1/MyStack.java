865915f8f95e3675893b543de44f7beafb81e861
ngavery <ang102@uottawa.ca>
2024-04-17 21:24:51 -0400

    .

--- Codes_PratiqueFinal/PartieII/Q1/MyStack.java
+++
@@ -1,8 +0,0 @@
-public interface MyStack {
-boolean isEmpty ( ) ;
-void push ( int  element ) ;
-int  pop ( ) ;
-int  peek ( ) ;
-int count ( ) ;
-void reverse ( ) ;
-}
--- Codes_PratiqueFinal/PartieII/Q3/Iterator.java
+++
@@ -1,8 +0,0 @@
-
-
-public interface Iterator<E>{
-// renvoie true s'il existe encore un élément à parcourir
-  public boolean hasNext();
-// renvoie l'élément courant et passe au suivant
-  public E next();
-}
